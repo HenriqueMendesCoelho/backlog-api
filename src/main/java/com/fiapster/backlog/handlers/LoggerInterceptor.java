511434c44dc2f4362c825832fb4e12a77be5056d
KronusBoss <henriquemendescoelho@gmail.com>
2022-12-08 02:15:51 -0300

    refactor(logging): reduce info logging to only afterCompletion

--- src/main/java/com/fiapster/backlog/handlers/LoggerInterceptor.java
+++ src/main/java/com/fiapster/backlog/handlers/LoggerInterceptor.java
@@ -12,25 +12,14 @@ public class LoggerInterceptor implements HandlerInterceptor {
 	private static Logger log = LoggerFactory.getLogger(LoggerInterceptor.class);
 	
 	@Override
-	public boolean preHandle(
-	  HttpServletRequest request,
-	  HttpServletResponse response, 
-	  Object handler) throws Exception {
-	    
-	    log.info("[preHandle]" + "[" + request.getMethod()
-	      + "]" + request.getRequestURI() + "[from:" + request.getHeader("x-forwarded-for") + "]");
-	    
-	    return true;
-	}
-	
-	@Override
 	public void afterCompletion(
 	  HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) 
 	  throws Exception {
 	    if (ex != null){
 	        ex.printStackTrace();
 	    }
-	    log.info("[afterCompletion][" + response.getStatus() + "]");
+	    log.info("[" + response.getStatus() + "]" + "[" + request.getMethod()
+	      + "]" + request.getRequestURI() + "[from:" + request.getHeader("x-forwarded-for") + "]");
 	}
 	
 	
