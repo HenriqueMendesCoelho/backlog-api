7743f33397a0414428e266e5e9284fa224c315d3
KronusBoss <henriquemendescoelho@gmail.com>
2022-12-08 00:48:34 -0300

    fix: Setting default timezone

--- src/main/java/com/fiapster/backlog/BacklogApplication.java
+++ src/main/java/com/fiapster/backlog/BacklogApplication.java
@@ -1,7 +1,11 @@
 package com.fiapster.backlog;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BacklogApplication {
@@ -9,5 +13,9 @@ public class BacklogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BacklogApplication.class, args);
 	}
-
+	
+	@PostConstruct
+	public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 }
